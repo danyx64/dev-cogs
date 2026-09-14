20018f9c1193f351706e33d29d7e1ab6c64f6c63
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-09-14 14:21:12 +0200

    Load simplified QuestTracker directly

--- questtracker/__init__.py
+++ questtracker/__init__.py
@@ -1,7 +1,5 @@
-from .v2 import install_v2_commands
-from .v23 import QuestTracker
+from .questtracker import QuestTracker
 
-install_v2_commands()
 
 async def setup(bot):
     await bot.add_cog(QuestTracker(bot))
