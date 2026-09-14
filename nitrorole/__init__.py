ef1d9e787efbdcb05494b70ba69313fab79ab08b
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-09-14 18:55:48 +0200

    Add NitroRole loader

---
+++ nitrorole/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2018-present Jakub Kuczys (https://github.com/Jackenmen)
+# Licensed under the Apache License, Version 2.0.
+
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement_or_raise
+
+from .nitrorole import NitroRole
+
+__red_end_user_data_statement__ = get_end_user_data_statement_or_raise(__file__)
+
+
+async def setup(bot: Red) -> None:
+    await bot.add_cog(NitroRole(bot))
