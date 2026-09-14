f54b6036a9283a42eaa77ea93b2e92d73de2c917
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-09-14 13:49:34 +0200

    Load QuestTracker v2 embed renderer

--- questtracker/__init__.py
+++ questtracker/__init__.py
@@ -1,4 +1,8 @@
-from .questtracker import QuestTracker
+from .v2 import QuestTracker, install_v2_commands
+
+
+install_v2_commands()
+
 
 async def setup(bot):
     await bot.add_cog(QuestTracker(bot))
