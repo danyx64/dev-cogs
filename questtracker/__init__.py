837a4b6756b796733f6f2b9ffe2dbaccb4f634cc
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-09-14 14:06:14 +0200

    Use QuestTracker v22

--- questtracker/__init__.py
+++ questtracker/__init__.py
@@ -1,9 +1,7 @@
 from .v2 import install_v2_commands
-from .v21 import QuestTracker
-
+from .v22 import QuestTracker
 
 install_v2_commands()
-
 
 async def setup(bot):
     await bot.add_cog(QuestTracker(bot))
