1b1fea4948ed5717c1638f8bbf2aeeb437746ef6
DanyX64 <124602162+danyx64@users.noreply.github.com>
2026-09-14 13:54:26 +0200

    Add QuestTracker placeholder aliases

---
+++ questtracker/template_placeholders.py
@@ -0,0 +1,13 @@
+EXTRA_PLACEHOLDERS = {
+    "title": "titolo della Quest",
+    "quest_title": "titolo della Quest",
+    "description": "descrizione della Quest",
+    "game_title": "titolo del gioco",
+    "app_id": "ID applicazione",
+    "orbs": "quantita di Orbs",
+    "server_name": "nome del server",
+    "server_id": "ID del server",
+    "remaining": "tempo rimanente",
+    "start_unix": "timestamp Unix inizio",
+    "end_unix": "timestamp Unix fine",
+}
